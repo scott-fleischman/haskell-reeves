4c44670957beee1d32d444a893b9fccbe3ec15f6
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-01-27 09:08:53 -0800

    Add sketch of config

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+
+data Config
+  = Config
+  { items :: HashMap Text Item
+  }
+
+data Item
+  = ServiceItem Service
+  | ReferenceItem Text
+
+data Service
+  = Service
+  { beforeStart :: [Command]
+  , start :: [Command]
+  , stop :: [Command]
+  }
+
+data Command
+  = ShellCommand Text
+  | DockerCommand Docker
+
+data Docker = Docker
+
 main :: IO ()
 main = do
   putStrLn "turtle"
