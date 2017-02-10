63c748715c234d9664aebdde58c3fb3fadea019c
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-02-10 09:48:59 -0800

    Add basic shell execution

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Data.Aeson ((.=))
@@ -5,18 +8,34 @@ import qualified Data.Aeson as Aeson
 import qualified Data.ByteString as Byte
 import           Data.Text (Text)
 import qualified Data.Text as Text
+import           Data.Text.Encoding (decodeUtf8)
 import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
+import qualified System.Process as Process
+import qualified System.Posix.Env.ByteString as Byte
 
-handleInput
+parseJson
   :: String
   -> (HashMap Text (HashMap Text Text) -> IO ())
   -> IO ()
-handleInput file f = do
+parseJson file f = do
   contents <- Byte.readFile file
   case Aeson.decodeStrict' contents of
     Nothing -> putStrLn "Unable to read file"
     Just top -> f top
 
+executeOrIgnore :: Text -> HashMap Text Text -> IO ()
+executeOrIgnore m t = case HashMap.lookup m t of
+  Nothing -> return ()
+  Just s -> do
+    let ss = Text.unpack s
+    putStrLn $ "> " ++ ss
+    code <- Process.runCommand ss >>= Process.waitForProcess
+    putStrLn $ show code
+    putStrLn ""
+
 main :: IO ()
-main = handleInput "example.json" (\_ -> putStrLn "Success")
+main = parseJson "reeves.json" $ \reeves -> do
+  (fmap . fmap) decodeUtf8 Byte.getArgs >>= \case
+    ["-a", s] -> mapM_ (executeOrIgnore s) reeves
+    _ -> putStrLn "Invalid arguments: use `reeves -a <cmd>` or `reeves <name> <cmd>`"
